d35f4ef52e52683a1ce29207e88f2a20fc80e3b0
rubensaxwiches <insert10lennieshere@gmail.com>
2020-03-03 15:33:42 -0800

    Autonomous
    Added encoders to the rest of the thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkingleftwall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkingleftwall.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous (name = "Parking(LeftWall)")
 public class parkingleftwall extends LinearOpMode {
@@ -25,57 +26,99 @@ public class parkingleftwall extends LinearOpMode {
         RightPull = hardwareMap.servo.get("LeftPull");
 
         waitForStart();
-        StrafeLeft(.6,1500);
+        goForward(1,2);
+        StrafeLeft(10,3);
+    }
+    private ElapsedTime runtime = new ElapsedTime();
 
-    }
+    static final double COUNTS_PER_MOTOR_REV = 537.6;
+    static final double WHEEL_DIAMETER_INCHES = 4.0;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.4;
+    static final double TURN_SPEED = 0.5;
 
-    public void goForward ( double power, int time)
-    {
-        RightF.setPower(-power);
-        RightB.setPower(-power);
-        LeftF.setPower(power);
-        LeftB.setPower(power);
-        sleep(time);
+    public void encoderMovement(double speed,
+                                double FrontLeftInches, double FrontRightInches, double BackLeftInches, double BackRightInches,
+                                double timeoutS) {
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
+
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = LeftF.getCurrentPosition() + (int) (FrontLeftInches * COUNTS_PER_INCH);
+            newBackLeftTarget = LeftB.getCurrentPosition() + (int) (BackLeftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = RightF.getCurrentPosition() + (int) (FrontRightInches * COUNTS_PER_INCH);
+            newBackRightTarget = RightB.getCurrentPosition() + (int) (BackRightInches * COUNTS_PER_INCH);
+
+            LeftF.setTargetPosition(newFrontLeftTarget);
+            LeftB.setTargetPosition(newBackLeftTarget);
+            RightF.setTargetPosition(newFrontRightTarget);
+            RightB.setTargetPosition(newBackRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            LeftF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LeftB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RightF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RightB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            LeftF.setPower(speed);
+            LeftB.setPower(speed);
+            RightF.setPower(speed);
+            RightB.setPower(speed);
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && LeftF.isBusy() && LeftB.isBusy() && RightF.isBusy() && RightB.isBusy()) {
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d");
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        LeftF.getCurrentPosition(),
+                        LeftB.getCurrentPosition());
+                RightF.getCurrentPosition();
+                RightB.getCurrentPosition();
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            LeftF.setPower(0);
+            LeftB.setPower(0);
+            RightF.setPower(0);
+            RightB.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            LeftF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            LeftB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            RightF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            RightB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
     }
-    public void goBackwards ( double power, int time)
+    public void StrafeLeft ( double inches, double timeoutS)
     {
-        RightF.setPower(power);
-        RightB.setPower(power);
-        LeftF.setPower(-power);
-        LeftB.setPower(-power);
-        sleep(time);
+        encoderMovement(DRIVE_SPEED, inches, -inches, -inches, inches, timeoutS);
     }
-    public void turnLeft ( double power, int time)
+    public void StrafeRight ( double inches, double timeoutS)
     {
-        RightF.setPower(power);
-        RightB.setPower(power);
-        LeftF.setPower(power);
-        LeftB.setPower(power);
-        sleep(time);
+        encoderMovement(DRIVE_SPEED, -inches, inches, inches, -inches, timeoutS);
     }
-    public void turnRight ( double power, int time)
+    public void turnRight ( double inches,double timeoutS)
     {
-        RightF.setPower(-power);
-        RightB.setPower(-power);
-        LeftF.setPower(-power);
-        LeftB.setPower(-power);
-        sleep(time);
+        encoderMovement(DRIVE_SPEED, -inches, inches, -inches, inches, timeoutS);
     }
-    public void StrafeRight ( double power, int time)
+    public void turnLeft ( double inches, double timeoutS)
     {
-        RightF.setPower(power);
-        RightB.setPower(-power);
-        LeftF.setPower(power);
-        LeftB.setPower(-power);
-        sleep(time);
+        encoderMovement(DRIVE_SPEED, inches, -inches, inches, -inches, timeoutS);
     }
-    public void StrafeLeft ( double power, int time)
+    public void goBackward ( double inches, double timeoutS)
     {
-        RightF.setPower(-power);
-        RightB.setPower(power);
-        LeftF.setPower(-power);
-        LeftB.setPower(power);
-        sleep(time);
+        encoderMovement(DRIVE_SPEED, inches, inches, inches, inches, timeoutS);
+    }
+    public void goForward(double inches, double timeoutS)
+    {
+        encoderMovement(DRIVE_SPEED,-inches,-inches,-inches,-inches,timeoutS);
     }
 
 }
